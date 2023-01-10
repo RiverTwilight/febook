a92035c56149593a44fce560e8c4b383be0affb2
RiverTwilight <wrj2014@126.com>
2023-01-10 22:35:07 +0800

    style: theme color

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
 	// GitHub pages deployment config.
 	// If you aren't using GitHub pages, you don't need these.
 	organizationName: "rivertwilight", // Usually your GitHub org/user name.
-	projectName: "fav", // Usually your repo name.
+	projectName: "febook", // Usually your repo name.
 
 	// Even if you don't use internalization, you can use this field to set useful
 	// metadata like html lang. For example, if your site is Chinese, you may want
@@ -54,10 +54,10 @@ const config = {
 		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
 		({
 			navbar: {
-				title: "René 的收藏夹",
+				title: "前端沉淀小册",
 				logo: {
 					alt: "My Site Logo",
-					src: "images/icon-192.png",
+					src: "icons/icon-192.png",
 				},
 				items: [
 					// {
